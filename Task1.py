a64c2070193a9a9a43f1f7a3ff29ffc13523d668
EkaterinaPavlova <malinakata@gmail.com>
2022-09-30 19:53:41 +0400

    save homework end

--- Task1.py
+++ Task1.py
@@ -3,13 +3,36 @@
 # - 6782 -> 23
 # - 0,56 -> 11
 
-number = float(input("Введите число: "))
-sum = 0
+number = float(input("Введите вещественное число: "))
+
+summa = 0
 number = number * 10 ** 4
 
 while number > 0:
     digit = number % 10
     if digit != 0:
-        sum = sum + digit
+        summa = summa + digit
     number = number // 10
-print(int(sum))
+
+print(int(summa))
+
+
+
+# def float_int(number_b: float) -> int:
+#     magic = int(1)
+#     temp = float(number_b)
+#     while not temp.is_integer():
+#         temp = number_b * magic
+#     return int(temp)
+#
+# def get_sum(number_a):
+#     no_point_num = float_int(number_a)
+#     no_point_num = abs(no_point_num)
+#     sum = 0
+#     while no_point_num > 0:
+#         sum += no_point_num % 10
+#         no_point_num //= 10
+#     return sum
+#
+# print(get_sum(number))
+
--- Task4.py
+++ Task4.py
@@ -1,17 +1,24 @@
 # 4.Задайте список из N элементов, заполненных числами из промежутка [-N, N].
 # Найдите произведение элементов на указанных позициях.
 
-
-
 import random
 
-numbers = []
 n = int(input("Введите размер списка: "))
-
+numbers = []
 for i in range(1, n + 1):
     numbers.append(random.randint(-10, 10))
+
 print(numbers)
 
-# pos1 = input("Введите позицию первого эл-та: ")
-# pos2 = input("Введите позицию второго эл-та: ")
+list = []
+
+with open("file.txt", "r") as data:
+    list = data.read().split("\n")
+
+average = numbers[int(list[0])] * numbers[int(list[7])]
+
+print(f"Приизведение элементов на позициях {list[0]} и {list[7]} равно {average}")
+
+
+
 
